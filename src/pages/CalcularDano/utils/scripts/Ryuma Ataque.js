f65c7ff76296b5ca93c71834a7234ca68e5ba5c7
Granodek <enzoyoung826@gmail.com>
2025-07-23 21:04:30 -0300

    sei la ta falando que o ataque ryuma ta modificado

--- src/pages/CalcularDano/utils/scripts/Ryuma Ataque.js
+++ src/pages/CalcularDano/utils/scripts/Ryuma Ataque.js
@@ -2,46 +2,40 @@ function RyumaAttack(dice, defense, accuracy, technique, strength, mastery, setN
     let acerto = Math.round(accuracy * 0.25);
     let raspao = Math.round(accuracy * 0.40);
     let blackFlash = Math.round(accuracy * 0.1);
-    
-    let percFisico = parseFloat(strength)|| 0;
-    let percTecnica = parseFloat(technique)|| 0;
-    let percVariaTecnica = 0.30;
-    let percVariaFisico = 0.70;
-    let percMastery = Number(mastery) || 0;
 
-    let danoArma = 15;
+    let percFisico = parseFloat(strength) || 0;
+    let percTecnica = parseFloat(technique) || 0;
+    let percMastery = Number(mastery) || 0;
     let dano;
 
     if (dice === 1) {
-        percTecnica *= percVariaTecnica * 2;
-        percFisico *= percVariaFisico * 2;
-        dano = percFisico + percTecnica + danoArma * 2;
+        percTecnica *= 2
+        percFisico *= 1.5   
+        dano = percFisico + percTecnica
         dano *= (1 + percMastery / 100);
-        dano = (dano/100) * (100-defense);
+        dano = Math.abs((dano/100) * (100-defense));
         setNewValueHit("BLACKFLASH!!!");
-        return `Dano Crítico de 200%: ${dano.toFixed(2)}`;
+        return `Critico Dano 200%: ${dano.toFixed(2)}`;
     } else if (dice <= blackFlash) {
-        percFisico *= percVariaFisico * 1.50;
-        percTecnica *= percVariaTecnica * 1.50;
-        dano = percFisico + percTecnica + danoArma;
+        percTecnica *= 1.5 
+        dano = (percFisico * 1.2) + percTecnica
         dano *= (1 + percMastery / 100);
-        dano = (dano/100) * (100-defense);
+        dano = Math.abs((dano/100) * (100-defense));
         setNewValueHit("BLACKFLASH!");
-        return `Dano Crítico: ${dano.toFixed(2)}`;
+        return `Critico Dano: ${dano.toFixed(2)}`;
     } else if (dice <= acerto) {
-        percTecnica *= percVariaTecnica;
-        percFisico *= percVariaFisico;
-        dano = percFisico + percTecnica + danoArma;
+        percTecnica *= 0.60
+        dano = percFisico + percTecnica
         dano *= (1 + percMastery / 100);
-        dano = (dano/100) * (100-defense);
+        dano = Math.abs((dano/100) * (100-defense));
         setNewValueHit("ACERTO");
         return `Dano de Acerto: ${dano.toFixed(2)}`;
     } else if (dice <= raspao) {
-        percFisico *= percVariaFisico/2
-        percTecnica *= percVariaFisico/2
-        dano = percFisico + percTecnica + danoArma / 2
+        percFisico *= 0.50
+        percTecnica *= 0.30
+        dano = percFisico + percTecnica
         dano *= (1 + percMastery / 100);
-        dano = (dano/100) * (100-defense);
+        dano = Math.abs((dano/100) * (100-defense));
         setNewValueHit("RASPAO");
         return `Dano de Raspão: ${dano.toFixed(2)}`;
     } else {
