be6f579614676167d5cbfaee3d73059ba58d7c03
Granodek <enzoyoung826@gmail.com>
2025-12-02 01:46:30 -0300

    buff dano energia CORRE

--- src/pages/CalcularDano/utils/scripts/Punho Corre.js
+++ src/pages/CalcularDano/utils/scripts/Punho Corre.js
@@ -2,19 +2,21 @@ function punhoCorre(dice, defense, accuracy, technique, strength, mastery, setNe
     let acerto = Math.round(accuracy * 0.25);
     let raspao = Math.round(accuracy * 0.40);
     let blackFlash = Math.round(accuracy * 0.1);
+
     if(blackFlash > 20){
         blackFlash = 20;
     }
+
     let percFisico = parseFloat(strength) || 0;
     let percTecnica = parseFloat(technique) || 0;
     let percMastery = Number(mastery) || 0;
 
     let dano;
-    let danoEnergia = percTecnica * 0.5;
+    let danoEnergia = (percTecnica * 0.75);
 
     if (dice === 1) {
-        percTecnica *= 2.40 
-        dano = (percFisico * 1.2) + percTecnica
+        percTecnica *= 2.40;
+        dano = (percFisico * 1.2) + percTecnica;
         dano *= (1 + percMastery / 100);
         dano = Math.abs((dano/100) * (100-defense));
         danoEnergia *= (1 + percMastery / 100);
@@ -23,16 +25,15 @@ function punhoCorre(dice, defense, accuracy, technique, strength, mastery, setNe
         return `Black Flash Dano: ${dano.toFixed(0)} e ${danoEnergia.toFixed(0)}`;
     } else if (dice <= blackFlash) {
         percTecnica *= 2.40 
-        dano = (percFisico * 1.2) + percTecnica
+        dano = (percFisico * 1.2) + percTecnica;
         dano *= (1 + percMastery / 100);
         dano = Math.abs((dano/100) * (100-defense));
         danoEnergia *= (1 + percMastery / 100);
         setNewValueHit("BLACKFLASH!");
         return `Black Flash Dano: ${dano.toFixed(0)} e ${danoEnergia.toFixed(0)}`;
     } else if (dice <= acerto) {
-        
         percTecnica *= 0.60;
-        dano = percFisico + percTecnica;
+        dano = (percFisico + percTecnica);
         dano *= (1 + percMastery / 100);
         dano = Math.abs((dano/100) * (100-defense));
         danoEnergia *= (1 + percMastery / 100);
