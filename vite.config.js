98a292f85123f96942861feebd9d371c66736eee
Granodek <enzoyoung826@gmail.com>
2026-02-13 12:41:36 -0300

    eita bixo2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: 'JujutsuRPG/',
   plugins: [react()],
 })
 //npm run build e depois npm run deploy para atualizar
